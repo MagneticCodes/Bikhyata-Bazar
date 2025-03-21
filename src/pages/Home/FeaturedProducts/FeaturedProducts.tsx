a44443596377b0e04e6a95d65908a46119bf9073
mohabbaticicle <mohabbatali@iciclecorporation.com>
2025-03-22 04:39:31 +0600

    updated product information

--- src/pages/Home/FeaturedProducts/FeaturedProducts.tsx
+++ src/pages/Home/FeaturedProducts/FeaturedProducts.tsx
@@ -1,42 +1,248 @@
 import React from 'react';
 
+import AISOLOVE_01 from  "../../../assets/Lalin Photo/AISOLOVE noF5 price-2200taka.webp"
+import AISOLOVE_02 from  "../../../assets/Lalin Photo/AISOLOVE noF5p max price-3000taka.webp"
+import Awei_F30 from  "../../../assets/Lalin Photo/Awei-F30-Mini-Desktop-Fan-1800taka.avif"
+import DP_7626 from  "../../../assets/Lalin Photo/DP-7626-Rechargeable-Table-Fan-With-Led-Light---4000Mah-box---700taka.jpg"
+import Double_ended from  "../../../assets/Lalin Photo/Double ended spray fan - 2200 taka.png"
+import Dp_duration from  "../../../assets/Lalin Photo/Dp duration power model-7638 --1400taka.webp"
+import Dp_7627 from  "../../../assets/Lalin Photo/Dp_7627_rechargeable_Portable_Usb_Fan_Wi-Non_Brand-2116a---1900taka.png"
+import FOLDING_FAN from  "../../../assets/Lalin Photo/FOLDING FAN MODEL-LR-2018--1000TAKA.jpg"
+import JISULIFE_MODEL from  "../../../assets/Lalin Photo/JISULIFE MODEL--FA26 --1700taka.jpg"
+import Led_fan from  "../../../assets/Lalin Photo/Led fan jr-2022 --1250taka.jpg"
+import Xundd_XDOT from  "../../../assets/Lalin Photo/Xundd-XDOT-024-USB-Rechargeable-Oscillating-Fan---2700taka.jpg"
+import air_cooler_fan from  "../../../assets/Lalin Photo/air cooler fan -1400taka.png"
+import arter_circulation_fan from  "../../../assets/Lalin Photo/carter circulation fan --3200taka.webp"
+import desktop_fan from  "../../../assets/Lalin Photo/desktop fan -2000taka.webp"
+import esktop_fan_m321 from  "../../../assets/Lalin Photo/desktop fan m321 2000taka.webp"
+import desktop_protocal from  "../../../assets/Lalin Photo/desktop protocal charger model-dp-7639---1250taka.jpg"
+import dp_7624 from  "../../../assets/Lalin Photo/dp-7624.jpg"
+import dp_7637_fan from  "../../../assets/Lalin Photo/dp-7637 fan -2000taka.webp"
+import dp_7637_2000Taka from  "../../../assets/Lalin Photo/dp-7637-2000Taka.webp"
+import dp_duration_power from  "../../../assets/Lalin Photo/dp-duration power-1800taka.jpg"
+import handle_fan_model from  "../../../assets/Lalin Photo/handle fan model-F2d--1500taka.jpg"
+import jisulife_table_fan from  "../../../assets/Lalin Photo/jisulife table fan7 m-FA13p --3600taka.jpg"
+import joykali_smd_rechargeable from  "../../../assets/Lalin Photo/joykali smd rechargeable led mini fan model-yg-729 --1350taka.webp"
+import jy_Super_jy_2218  from  "../../../assets/Lalin Photo/jy Super jy-2218   -1900taka.jpg"
+import jy_super_model_jy_2523  from  "../../../assets/Lalin Photo/jy super model-jy-2523 --1400taka.avif"
+import jy_1881_1100taka  from  "../../../assets/Lalin Photo/jy-1881 -1100taka.jpg"
+import mini_handle_fan  from  "../../../assets/Lalin Photo/mini handle fan model-fa20 -- 1100taka.jpg"
+import mini_turbo_fan  from  "../../../assets/Lalin Photo/mini turbo fan --model-ym88154-- 1550taka.jpg"
+import supermoon_protable  from  "../../../assets/Lalin Photo/supermoon protable mini fan model-sm6615 --2500taka.jpg"
+import xpower_6  from  "../../../assets/Lalin Photo/xpower 6'' mini fan taka-900.jpg"
+import xpower_7 from  "../../../assets/Lalin Photo/xpower 7'' us mini fan--1050taka.jpg"
+import xpower_wd_202 from  "../../../assets/Lalin Photo/xpower wd-202 -- 1350taka.webp"
+
 const FeaturedProducts: React.FC = () => {
   const products = [
     {
-      title: 'Winter Long Sleeve Black White',
-      price: 400.00,
-      oldPrice: 580.00,
-      image: 'https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg',
-      tag: 'SALE'
+      title: 'AISOLOVE No F5',
+      price: 2200.00,
+      oldPrice: 2500.00,
+      image: AISOLOVE_01,
+      tag: 'POPULAR'
     },
     {
-      title: 'Women Totes Lady Handbag',
-      price: 300.00,
-      image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg',
+      title: 'AISOLOVE No F5P Max',
+      price: 3000.00,
+      oldPrice: 3500.00,
+      image: AISOLOVE_02,
     },
     {
-      title: 'Lace Water Soluble Sexy Dress',
-      price: 600.00,
-      image: 'https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg',
+      title: 'Awei F30 Mini Desktop Fan',
+      price: 1800.00,
+      oldPrice: 2200.00,
+      image: Awei_F30,
       tag: 'NEW'
     },
     {
-      title: 'Casual Summer Dress',
-      price: 450.00,
-      oldPrice: 599.00,
-      image: 'https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg',
+      title: 'DP-7626 Rechargeable Table Fan With LED Light',
+      price: 700.00,
+      oldPrice: 950.00,
+      image: DP_7626,
       tag: 'SALE'
     },
     {
-      title: 'Leather Crossbody Bag',
-      price: 350.00,
-      image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg',
+      title: 'Double Ended Spray Fan',
+      price: 2200.00,
+      oldPrice: 2600.00,
+      image: Double_ended,
+    },
+    {
+      title: 'DP Duration Power Model-7638',
+      price: 1400.00,
+      oldPrice: 1600.00,
+      image: Dp_duration,
       tag: 'NEW'
     },
     {
-      title: 'Evening Cocktail Dress',
-      price: 750.00,
-      image: 'https://images.pexels.com/photos/985635/pexels-photo-985635.jpeg',
+      title: 'DP-7627 Rechargeable Portable USB Fan',
+      price: 1900.00,
+      oldPrice: 2200.00,
+      image: Dp_7627,
+    },
+    {
+      title: 'Folding Fan Model LR-2018',
+      price: 1000.00,
+      oldPrice: 1350.00,
+      image: FOLDING_FAN,
+      tag: 'SALE'
+    },
+    {
+      title: 'JISULIFE Model FA26',
+      price: 1700.00,
+      oldPrice: 1950.00,
+      image: JISULIFE_MODEL,
+    },
+    {
+      title: 'LED Fan JR-2022',
+      price: 1250.00,
+      oldPrice: 1500.00,
+      image: Led_fan,
+      tag: 'NEW'
+    },
+    {
+      title: 'Xundd XDOT-024 USB Rechargeable Oscillating Fan',
+      price: 2700.00,
+      oldPrice: 3100.00,
+      image: Xundd_XDOT,
+    },
+    {
+      title: 'Air Cooler Fan',
+      price: 1400.00,
+      oldPrice: 1800.00,
+      image: air_cooler_fan,
+      tag: 'POPULAR'
+    },
+    {
+      title: 'Carter Circulation Fan',
+      price: 3200.00,
+      oldPrice: 3600.00,
+      image: arter_circulation_fan,
+      tag: 'PREMIUM'
+    },
+    {
+      title: 'Desktop Fan',
+      price: 2000.00,
+      oldPrice: 2300.00,
+      image: desktop_fan,
+    },
+    {
+      title: 'Desktop Fan M321',
+      price: 2000.00,
+      oldPrice: 2400.00,
+      image: esktop_fan_m321,
+    },
+    {
+      title: 'Desktop Protocol Charger Model-DP-7639',
+      price: 1250.00,
+      oldPrice: 1450.00,
+      image: desktop_protocal,
+      tag: 'NEW'
+    },
+    {
+      title: 'DP-7624 Fan',
+      price: 1800.00, 
+      oldPrice: 2100.00,
+      image: dp_7624,
+    },
+    {
+      title: 'DP-7637 Fan',
+      price: 2000.00,
+      oldPrice: 2300.00,
+      image: dp_7637_fan,
+    },
+    {
+      title: 'DP-7637 Fan',
+      price: 2000.00,
+      oldPrice: 2350.00,
+      image: dp_7637_2000Taka,
+      tag: 'BESTSELLER'
+    },
+    {
+      title: 'DP Duration Power Fan',
+      price: 1800.00,
+      oldPrice: 2050.00,
+      image: dp_duration_power,
+    },
+    {
+      title: 'Handle Fan Model-F2D',
+      price: 1500.00,
+      oldPrice: 1850.00,
+      image: handle_fan_model,
+      tag: 'POPULAR'
+    },
+    {
+      title: 'Jisulife Table Fan 7 M-FA13P',
+      price: 3600.00,
+      oldPrice: 4000.00,
+      image: jisulife_table_fan,
+      tag: 'PREMIUM'
+    },
+    {
+      title: 'Joykali SMD Rechargeable LED Mini Fan Model-YG-729',
+      price: 1350.00,
+      oldPrice: 1550.00,
+      image: joykali_smd_rechargeable,
+    },
+    {
+      title: 'JY Super JY-2218',
+      price: 1900.00,
+      oldPrice: 2150.00,
+      image: jy_Super_jy_2218,
+    },
+    {
+      title: 'JY Super Model-JY-2523',
+      price: 1400.00,
+      oldPrice: 1650.00,
+      image: jy_super_model_jy_2523,
+      tag: 'SALE'
+    },
+    {
+      title: 'JY-1881 Fan',
+      price: 1100.00,
+      oldPrice: 1300.00,
+      image: jy_1881_1100taka,
+    },
+    {
+      title: 'Mini Handle Fan Model-FA20',
+      price: 1100.00,
+      oldPrice: 1350.00,
+      image: mini_handle_fan,
+      tag: 'SALE'
+    },
+    {
+      title: 'Mini Turbo Fan Model-YM88154',
+      price: 1550.00,
+      oldPrice: 1800.00,
+      image: mini_turbo_fan,
+    },
+    {
+      title: 'Supermoon Portable Mini Fan Model-SM6615',
+      price: 2500.00,
+      oldPrice: 2900.00,
+      image: supermoon_protable,
+      tag: 'POPULAR'
+    },
+    {
+      title: 'XPower 6" Mini Fan',
+      price: 900.00,
+      oldPrice: 1100.00,
+      image: xpower_6,
+      tag: 'SALE'
+    },
+    {
+      title: 'XPower 7" US Mini Fan',
+      price: 1050.00,
+      oldPrice: 1250.00,
+      image: xpower_7,
+    },
+    {
+      title: 'XPower WD-202',
+      price: 1350.00,
+      oldPrice: 1600.00,
+      image: xpower_wd_202,
+      tag: 'NEW'
     }
   ];
 
--- src/pages/Home/NewProducts/NewProducts.tsx
+++ src/pages/Home/NewProducts/NewProducts.tsx
@@ -2,28 +2,73 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
 
+import AISOLOVE_01 from "../../../assets/Lalin Photo/AISOLOVE noF5 price-2200taka.webp";
+import Awei_F30 from "../../../assets/Lalin Photo/Awei-F30-Mini-Desktop-Fan-1800taka.avif";
+import Led_fan from "../../../assets/Lalin Photo/Led fan jr-2022 --1250taka.jpg";
+import desktop_protocal from "../../../assets/Lalin Photo/desktop protocal charger model-dp-7639---1250taka.jpg";
+import Dp_duration from "../../../assets/Lalin Photo/Dp duration power model-7638 --1400taka.webp";
+import xpower_wd_202 from "../../../assets/Lalin Photo/xpower wd-202 -- 1350taka.webp";
+import joykali_smd_rechargeable from "../../../assets/Lalin Photo/joykali smd rechargeable led mini fan model-yg-729 --1350taka.webp";
+import jy_super_model_jy_2523 from "../../../assets/Lalin Photo/jy super model-jy-2523 --1400taka.avif";
+
 const NewProducts: React.FC = () => {
   const products = [
     {
-      title: "Women White Backless Mini",
-      price: 400.0,
-      oldPrice: 580.0,
-      image:
-        "https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg",
-      tag: "SALE",
+      title: "AISOLOVE No F5",
+      price: 2200.00,
+      oldPrice: 2500.00,
+      image: AISOLOVE_01,
+      tag: "POPULAR",
     },
     {
-      title: "Women Totes Lady Handbag",
-      price: 300.0,
-      image:
-        "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg",
-    },
-    {
-      title: "Sexy Women Floral Embroidery",
-      price: 600.0,
-      image: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg",
+      title: "Awei F30 Mini Desktop Fan",
+      price: 1800.00,
+      oldPrice: 2200.00,
+      image: Awei_F30,
       tag: "NEW",
     },
+    {
+      title: "LED Fan JR-2022",
+      price: 1250.00,
+      oldPrice: 1500.00,
+      image: Led_fan,
+      tag: "NEW",
+    },
+    {
+      title: "Desktop Protocol Charger Model-DP-7639",
+      price: 1250.00,
+      oldPrice: 1450.00,
+      image: desktop_protocal,
+      tag: "NEW",
+    },
+    {
+      title: "DP Duration Power Model-7638",
+      price: 1400.00,
+      oldPrice: 1600.00,
+      image: Dp_duration,
+      tag: "NEW",
+    },
+    {
+      title: "XPower WD-202",
+      price: 1350.00,
+      oldPrice: 1600.00,
+      image: xpower_wd_202,
+      tag: "NEW",
+    },
+    {
+      title: "Joykali SMD Rechargeable LED Mini Fan",
+      price: 1350.00,
+      oldPrice: 1550.00,
+      image: joykali_smd_rechargeable,
+      tag: "NEW",
+    },
+    {
+      title: "JY Super Model-JY-2523",
+      price: 1400.00,
+      oldPrice: 1650.00,
+      image: jy_super_model_jy_2523,
+      tag: "NEW",
+    }
   ];
 
   return (
@@ -36,9 +81,9 @@ const NewProducts: React.FC = () => {
           modules={[Pagination, Navigation]}
           spaceBetween={30}
           slidesPerView={1}
+          navigation
           pagination={{
             clickable: true,
-            bulletClass: "swiper-pagination-bullet bg-emerald-400",
           }}
           breakpoints={{
             640: { slidesPerView: 2 },
@@ -48,18 +93,20 @@ const NewProducts: React.FC = () => {
         >
           {products.map((product, index) => (
             <SwiperSlide key={index}>
-              <div className="group">
-                <div className="relative overflow-hidden rounded-lg mb-4">
+              <div className="group bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300">
+                <div className="relative overflow-hidden h-64">
                   <img
                     src={product.image}
                     alt={product.title}
-                    className="w-full h-96 object-cover transform group-hover:scale-110 transition-transform duration-500"
+                    className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500"
                   />
                   {product.tag && (
                     <span
-                      className={`absolute top-4 left-4 px-4 py-1 rounded-full text-white text-sm font-medium
-                      ${
-                        product.tag === "SALE" ? "bg-red-500" : "bg-green-500"
+                      className={`absolute top-4 left-4 px-4 py-1 rounded-full text-white text-sm font-medium ${
+                        product.tag === "SALE" ? "bg-red-500" : 
+                        product.tag === "NEW" ? "bg-green-500" : 
+                        product.tag === "PREMIUM" ? "bg-purple-500" :
+                        product.tag === "BESTSELLER" ? "bg-yellow-500" : "bg-blue-500"
                       }`}
                     >
                       {product.tag}
@@ -67,21 +114,23 @@ const NewProducts: React.FC = () => {
                   )}
                 </div>
 
-                <h3 className="text-lg font-medium mb-2">{product.title}</h3>
-                <div className="flex items-center gap-2">
-                  <span className="text-emerald-600 font-bold">
-                    ${product.price.toFixed(2)}
-                  </span>
-                  {product.oldPrice && (
-                    <span className="text-gray-400 line-through">
-                      ${product.oldPrice.toFixed(2)}
+                <div className="p-4">
+                  <h3 className="text-lg font-medium mb-2 h-14 overflow-hidden">{product.title}</h3>
+                  <div className="flex items-center gap-2">
+                    <span className="text-emerald-600 font-bold">
+                      ${product.price.toFixed(2)}
                     </span>
-                  )}
-                </div>
+                    {product.oldPrice && (
+                      <span className="text-gray-400 line-through">
+                        ${product.oldPrice.toFixed(2)}
+                      </span>
+                    )}
+                  </div>
 
-                <button className="w-full mt-4 px-6 py-2 bg-gray-900 text-white rounded hover:bg-emerald-500 transition-colors duration-300">
-                  ADD TO CART
-                </button>
+                  <button className="w-full mt-4 px-6 py-2 bg-gray-900 text-white rounded hover:bg-emerald-500 transition-colors duration-300">
+                    ADD TO CART
+                  </button>
+                </div>
               </div>
             </SwiperSlide>
           ))}
